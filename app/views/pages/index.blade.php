06e35a3356396ae1d29774e7ac072f78e1f03119
Andrew Allbright <aallbrig@Andrews-MacBook-Pro.local>
2014-12-14 18:34:54 -0500

    Modified index page to account for much-reduced requirement load

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h1>Andrew Allbright's N413 Assignments</h1>
     <p>These are a list of assignments completed for IUPUI's New Media Advanced Web Fall 2014 course with Skip Comer.  This has just a little bootstrap and uses Laravel 4 on the backend just to make the site a little more interesting! <a href="https://github.com/aallbrig/n413-exercises">Source is up on github!</a></p>
+    <p>Requirement: do 6 exercises</p>
     <h3>Exercises</h3>
     <ol>
       <li><a href="exercises/1">Menu exercise</a></li>
@@ -10,12 +11,6 @@
       <li><a href="exercises/3">Sub Menu With Content exercise</a></li>
       <li><a href="exercises/4">Log in exercise</a></li>
       <li><a href="exercises/5">Session expiration exercise</a></li>
-      <li><a href="exercises/6">Password Encryption and Account Creation</a></li>
       <li><a href="exercises/7">Data-driven Canvas Tag!</a></li>
-      <li><a href="exercises/8">Password Recovery System</a></li>
-      <li><a href="exercises/9">Geolocation(?) Exercise</a></li>
-      <li><a href="exercises/10">Four square + Geolocation</a></li>
-      <li><a href="exercises/11">Image jQuery Slider</a></li>
-      <li><a href="exercises/12">Google Analytics</a></li>
     </ol>
 @stop
