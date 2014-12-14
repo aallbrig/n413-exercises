46340984b702368b03fe7e27fcb384e78503ca7e
Andrew Allbright <aallbrig@Andrews-MacBook-Pro.local>
2014-12-14 18:39:27 -0500

    Cleaned up exercise 7 javascript file of all commented out work.  Added console.log statements to figure out what is going wrong in production

--- public/js/7.js
+++ public/js/7.js
@@ -1,48 +1,31 @@
 var CanvasExercise = function(canvasId, entityCollection){
-  var that = this;
+  var _this = this;
 
   this.entityCollection = entityCollection || [];
-  console.log(this.entityCollection.length);
   this.updateMotion = function(){
-    that.ct.clearRect(0, 0, that.canvas.width(), that.canvas.height());
-    that.entityCollection.map(function(entity){
-      if(entity.x > that.canvas.width() || entity.x < 0){
+    console.log('update motion called');
+    _this.ct.clearRect(0, 0, _this.canvas.width(), _this.canvas.height());
+    _this.entityCollection.map(function(entity){
+      if(entity.x > _this.canvas.width() || entity.x < 0){
         entity.dx = -entity.dx;
       }
-      if(entity.y > that.canvas.height() || entity.y < 0){
+      if(entity.y > _this.canvas.height() || entity.y < 0){
         entity.dy = -entity.dy;
       }
       entity.x+=entity.dx, entity.y+=entity.dy;
-      that.ct.beginPath();
-      that.ct.arc(entity.x,entity.y,entity.radius,0,2*Math.PI);
-      that.ct.stroke();
+      _this.ct.beginPath();
+      _this.ct.arc(entity.x,entity.y,entity.radius,0,2*Math.PI);
+      _this.ct.stroke();
     });
   }
   this.serverSync = function(){
-    $.post('/exercises/7', {"canvasEntities":that.entityCollection}, "json");
-    // $.post('/exercises/7', that.entityCollection, "json");
-   //  $.ajax({
-   //    type: "POST",
-   //    contentType: "application/json",
-   //    url: '/exercises/7',
-   //    data: {"canvasEntities":that.entityCollection},
-   //    dataType: "json"
-   // });
-    // $.ajax({
-    //     url: '/exercises/7',
-    //     type: 'POST',
-    //     dataType: 'json',
-    //     data: {"canvasEntities":that.entityCollection},
-    //     contentType: 'application/json; charset=utf-8',
-    //     success: function(result) {
-    //         // alert(result.Result);
-    //     }
-    // });
+    console.log('server sync called');
+    $.post('/exercises/7', {"canvasEntities":_this.entityCollection}, "json");
   }
   $(function(){
-    that.canvas = $(canvasId);
-    that.ct = that.canvas[0].getContext('2d');
-    setInterval(that.updateMotion, 33);
-    setInterval(that.serverSync, 5000);
+    _this.canvas = $(canvasId);
+    _this.ct = _this.canvas[0].getContext('2d');
+    setInterval(_this.updateMotion, 33);
+    setInterval(_this.serverSync, 5000);
   });
 }
