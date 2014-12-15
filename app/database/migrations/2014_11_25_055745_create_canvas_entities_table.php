f41ea4a87b15244f198b36d6faf1dd6a6d7b39fc
Andrew Allbright <aallbrig@Andrews-MacBook-Pro.local>
2014-12-14 22:44:04 -0500

    Changed to string data type

--- app/database/migrations/2014_11_25_055745_create_canvas_entities_table.php
+++ app/database/migrations/2014_11_25_055745_create_canvas_entities_table.php
@@ -16,11 +16,11 @@ class CreateCanvasEntitiesTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('name');
-			$table->decimal('x');
-			$table->decimal('y');
-			$table->decimal('dx');
-			$table->decimal('dy');
-			$table->decimal('radius');
+			$table->string('x');
+			$table->string('y');
+			$table->string('dx');
+			$table->string('dy');
+			$table->string('radius');
 			$table->timestamps();
 		});
 	}
